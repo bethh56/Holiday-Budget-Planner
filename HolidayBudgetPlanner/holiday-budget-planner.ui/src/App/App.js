acd085af3aec2e184092b4b949a540820365da65
Beth <bethhnielsen56@gmail.com>
2020-11-29 18:56:42 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner.ui/src/App/App.js
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/App/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+import MyNavbar from '../Components/shared/MyNavbar/MyNavbar';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Holiday Budget Planner</h1>
-      <button className="btn btn-info">I am a button</button>
+       <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar/>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+class MyNavbar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  render() {
+    const { isOpen } = this.state;
+
+    const toggle = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    };
+
+    return (
+      <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Holiday Budget Planner</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+    );
+  }
+}
+
+export default MyNavbar;
