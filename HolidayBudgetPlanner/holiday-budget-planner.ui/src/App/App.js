110c5aadf66e50fd19ef147e6fff1d602f14da93
Beth <bethhnielsen56@gmail.com>
2020-11-29 18:25:52 -0600

    set up complete

---
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Holiday Budget Planner</h1>
+      <button className="btn btn-info">I am a button</button>
+    </div>
+  );
+}
+
+export default App;
