38a8531c29bdb590f8405f0b0b3a8dc873747f5f
Beth <bethhnielsen56@gmail.com>
2020-12-01 18:57:11 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Program.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace HBP
+namespace holiday_budget_planner
 {
     public class Program
     {
