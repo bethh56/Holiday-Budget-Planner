7b45a1a2533be5fac33a1a2b882df3cc25b01a3f
Beth <bethhnielsen56@gmail.com>
2020-12-01 20:53:51 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/BudgetController.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/BudgetController.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using holiday_budget_planner.DataAccess;
+using holiday_budget_planner.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace holiday_budget_planner.Controllers
 {
-    public class BudgetController
+    [Route("api/budget")]
+    [ApiController]
+    public class BudgetController : ControllerBase
     {
+        BudgetRepository _budgetRepo;
+        public BudgetController()
+        {
+            _budgetRepo = new BudgetRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetCurrentPlan()
+        {
+            var currentPlanBudget = _budgetRepo.GetCurrentPlan();
+
+            return Ok(currentPlanBudget);
+
+        }
+
     }
 }
--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/DataAccess/BudgetRepository.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/DataAccess/BudgetRepository.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using holiday_budget_planner.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace holiday_budget_planner.DataAccess
 {
     public class BudgetRepository
     {
+        static List<Budget> budget = new List<Budget>();
+
+        const string _connectionString = "Server=localhost;Database=HolidayBudgetPlanner;Trusted_Connection=True";
+
+        public IEnumerable<Budget> GetCurrentPlan()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"select *
+                        from Budget";
+
+            var currentBudgetPlan = db.Query<Budget>(sql);
+
+            return currentBudgetPlan;
+
+        }
     }
 }
