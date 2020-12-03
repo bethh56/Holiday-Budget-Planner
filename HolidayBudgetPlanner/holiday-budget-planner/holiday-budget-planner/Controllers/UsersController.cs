5d35e0660cbfae4cc00126a6874304f56f304e4b
Beth <bethhnielsen56@gmail.com>
2020-12-03 13:07:55 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/UsersController.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/UsersController.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using holiday_budget_planner.DataAccess;
+using holiday_budget_planner.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace holiday_budget_planner.Controllers
 {
-    public class UsersController
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
+        UsersRepository _userRepo;
+        public UsersController()
+        {
+            _userRepo = new UsersRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var allUsers = _userRepo.GetAllUsers();
+
+            return Ok(allUsers);
+
+        }
     }
 }
--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/DataAccess/UsersRepository.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/DataAccess/UsersRepository.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using holiday_budget_planner.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace holiday_budget_planner.DataAccess
 {
     public class UsersRepository
     {
+        static List<Users> user = new List<Users>();
+
+        const string _connectionString = "Server=localhost;Database=HolidayBudgetPlanner;Trusted_Connection=True";
+
+        public IEnumerable<Users> GetAllUsers()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"select *
+                        from Users";
+
+            var users = db.Query<Users>(sql);
+
+            return users;
+
+        }
     }
 }
