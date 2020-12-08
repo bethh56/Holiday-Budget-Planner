7b2d7b88ab99cd5246eb564b76c0d45babfdeb0b
Beth <bethhnielsen56@gmail.com>
2020-12-07 19:49:45 -0600

    merged in master

--- HolidayBudgetPlanner/holiday-budget-planner.ui/src/helpers/data/itemData.js
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/helpers/data/itemData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { baseUrl } from './constants.json';
+import { baseUrl } from '../constants.json';
 
 const getBudgetItems = (userId) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/ItemCategory/user${userId}`)
