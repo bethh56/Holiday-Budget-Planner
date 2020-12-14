63da49c8a4b0bd68c85c1fb54b22e47206240a79
Beth <bethhnielsen56@gmail.com>
2020-12-13 18:30:43 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/pages/AddNewBudget/AddNewBudget.js
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/pages/AddNewBudget/AddNewBudget.js
@@ -1,22 +1,88 @@
 import React from 'react';
-// import budgetData from '../../../helpers/data/budgetData';
+import budgetData from '../../../helpers/data/budgetData';
+import holidayData from '../../../helpers/data/holidayData';
 import './AddNewBudget.scss';
 
 class AddNewBudget extends React.Component {
   state = {
     holidayId: '',
     budgetAmount: '',
-    currentPlan: false,
+    userId: '',
+    holiday: [],
+    dateCreated: '',
   }
 
-  // componentDidMount() {
-  //   this.createNewBudget();
-  // }
+  budget = (e) => {
+    e.preventDefault();
+    this.setState({ budgetAmount: e.target.value });
+  }
+
+  setHoliday = (e) => {
+    e.preventDefault();
+    this.setState({ holidayId: e.target.value });
+  }
+
+  saveNewBudget = (e) => {
+    e.preventDefault();
+    const {
+      holidayId,
+      budgetAmount,
+      userId,
+      dateCreated,
+    } = this.state;
+
+    const newItem = {
+      holidayById: holidayId,
+      plannedBudget: budgetAmount,
+      2: userId,
+      date: dateCreated,
+    };
+    budgetData.addNewBudget(newItem)
+      .then(() => this.props.history.push('/home'))
+      .catch((err) => console.error('unable to save new budget', err));
+  }
+
+  displayHolidayOptions = () => {
+    holidayData.getHolidays()
+      .then((holiday) => this.setState({ holiday }))
+      .catch((err) => console.error('unable to get holiday info'));
+  }
+
+  componentDidMount() {
+    this.displayHolidayOptions();
+  }
 
   render() {
+    const {
+      holiday,
+      budgetAmount,
+      holidayId,
+    } = this.state;
     return (
       <div className="addNewBudget">
-        <h1>Add New Budget</h1>
+        <h1>Create New Budget</h1>
+        <form>
+        <label>
+          Select a Holiday
+          <select>
+            {
+              holiday.map((h, indx) => (
+                <option value={holidayId} onChange={this.setHoliday}>{h.holidayName}</option>
+              ))
+            }
+          </select>
+        </label>
+        <label>
+          Planned Budget Amount:
+          <input
+          type="text"
+          className="form-control"
+          id="item-name"
+          value={budgetAmount}
+          onChange={this.budget}/>
+        </label>
+        <button classname="btn btn-danger" onClick={this.saveNewBudget}>Submit</button>
+      </form>
       </div>
     );
   }
--- HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/HolidayController.cs
+++ HolidayBudgetPlanner/holiday-budget-planner/holiday-budget-planner/Controllers/HolidayController.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using holiday_budget_planner.DataAccess;
+using holiday_budget_planner.Models;
+using Microsoft.AspNetCore.Authorization;
 namespace holiday_budget_planner.Controllers
 {
-    public class HolidayController
+    [Route("api/holiday")]
+    [ApiController]
+    public class HolidayController : ControllerBase
     {
+        HolidayRepository _holidayRepo;
+        public HolidayController()
+        {
+            _holidayRepo = new HolidayRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHolidays()
+        {
+            var holiday = _holidayRepo.GetAllHolidays();
+
+            return Ok(holiday);
+        }
     }
 }
