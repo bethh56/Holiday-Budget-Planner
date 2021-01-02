be08ca36e0838dae8fc0f83c536b76bf2593d2ca
Beth <bethhnielsen56@gmail.com>
2021-01-01 23:49:43 -0600

    wip

--- HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/shared/BudgetDetails/BudgetDetails.js
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/shared/BudgetDetails/BudgetDetails.js
@@ -6,10 +6,9 @@ class BudgetDetails extends React.Component {
     const { budgetPlan } = this.props;
     return (
       <div className='budgetDetails'>
-        <h1>Budget Amount</h1>
-        <h2>${budgetPlan.budgetAmount}</h2>
-        <h2>{budgetPlan.holidayName}</h2>
-        <h2>BudgetId: {budgetPlan.id}</h2>
+        <h1 className="holidayName">{budgetPlan.holidayName}</h1>
+        <h4>Amount Left To Spend:</h4>
+        <h1>${budgetPlan.budgetAmount}</h1>
       </div>
     );
   }
