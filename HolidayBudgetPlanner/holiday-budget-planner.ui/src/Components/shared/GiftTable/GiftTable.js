862eadb8a4bfe25cec997823713247f342c37e78
Beth <bethhnielsen56@gmail.com>
2021-01-02 18:52:22 -0600

    styled gift table

--- HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/shared/GiftTable/GiftTable.js
+++ HolidayBudgetPlanner/holiday-budget-planner.ui/src/Components/shared/GiftTable/GiftTable.js
@@ -12,8 +12,7 @@ class GiftTable extends React.Component {
     const { item, removeGift, giftLineItem } = this.props;
     return (
       <div className="BudgetItemTable">
-        <h4>Purchased Gifts</h4>
-        <h5 className="amountSpent">Amount Spent: ${item.totalPrice}</h5>
+        <h5>Purchased Gifts Total: <span className="dollarAmount">${item.totalPrice}</span></h5>
         <Table>
       <thead>
         <tr>
@@ -29,7 +28,7 @@ class GiftTable extends React.Component {
           <td><p key={indx}>{i.recepient}</p></td>
           <td><p key={indx}>{i.item}</p></td>
           <td><p key={indx}>${i.price}</p></td>
-          <td> <button key={indx} className="btn btn-danger" onClick={() => removeGift(i.id)}><i className="fas fa-trash-alt"></i></button> </td>
+          <td> <button key={indx} className="deleteGiftBtn" onClick={() => removeGift(i.id)}><i className="fas fa-trash-alt"></i></button> </td>
           </tr>
           ))}
         </tbody>
